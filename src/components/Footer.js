b0531f1337b7da2d4c88859354bc8aeca2f21bab
Celestine <celestine4321@gmail.com>
2025-08-18 19:59:54 +0100

    Updated my instagram handle

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/jesuiscelestine__/"
+                href="https://www.instagram.com/_celesteen/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/jesuiscelestine__/"
+                  href="https://www.instagram.com/_celesteen/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
